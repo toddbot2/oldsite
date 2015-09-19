6bebbc83fb31b1bc02dc223c2b53b52d3da3b842
Todd <toddbot@Todds-MacBook-Pro.local>
2015-09-19 01:35:36 -0700

    header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-$(document.ready(function(){
-	$('.navbar').click(function(){
-		var list = $('.navbar');
-		if (list.hasClass('expand'))
-			list.removeClass('expand'));
-
-		else 
+$(document).ready(function(){
+    console.log("ready.........");
+	$('.nav-bar').click(function(){
+        console.log("clicked!!!!!!!");
+		var list = $('.nav-bar');
+		if (list.hasClass('expand')) {
+            console.log('removing!!!');
+			list.removeClass('expand');
+		} else {
+            console.log('adding!!!!!!!!111');
 			list.addClass('expand');
+        }
 	});
-
+	$('.parent').click(function(){
+		var proj = $('.drop_down', this);
+		console.log('is this thing on?');
+		if (proj.hasClass('expand')){
+			proj.removeClass('expand');
+		}
+		 else{
+			proj.addClass('expand');
+		}
+	});
 });
-
-	
